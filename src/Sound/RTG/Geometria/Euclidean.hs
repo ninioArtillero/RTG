a258dc13db32eb6e0a5e4805f50cdac5fb5aa854
ninioArtillero <ixbalanque@protonmail.ch>
2023-06-07 15:01:41 -0600

    prettify code

--- src/Sound/RTG/Geometria/Euclidean.hs
+++ src/Sound/RTG/Geometria/Euclidean.hs
@@ -1,9 +1,10 @@
-type Euclidean = (
-  Int, -- pulsos
-  Int, -- eventos
-  Int  -- rotación
-  )
+module Sound.RTG.Geometria.Euclidean where
 
+type Euclidean =
+  ( Int, -- pulsos
+    Int, -- eventos
+    Int  -- rotación
+  )
 
 -- La siguiente función auxiliar reduce la representación de un ritmo
 -- de manera que para [(x,y,z) | x >=0, y >= 0, z >= 0] se cumpla
@@ -15,9 +16,10 @@ type Euclidean = (
 reduceR :: Euclidean -> Euclidean
 reduceR (x, y, z) = (x', y', z')
   where
-    x' = if x `mod` y == 0
-      then y -- ritmos isócronos
-      else x `mod` y
+    x' =
+      if x `mod` y == 0
+        then y -- ritmos isócronos
+        else x `mod` y
     y' = y
     z' = z `mod` y
 
@@ -26,28 +28,25 @@ reduceR (x, y, z) = (x', y', z')
 -- (3,3), (3,6), (3,9), etc.
 -- (5,5), (5,10), (5,15), etc.
 
-
 -- TODO: reescribir usando guards
 (>+<) :: Euclidean -> Euclidean -> Euclidean -- type declaration
-(x, y, z) >+< (a, b, c)=                     -- function definition
-  if and [y/=0, b/=0]
-    then                                     -- output expression:
-      (
-      (x + a) `mod` w,                       -- pulses
-      w,                                     -- events
-      (z' + c') `mod` w                      -- rotation
+(x, y, z) >+< (a, b, c) =
+  -- function definition
+  if (y /= 0) && (b /= 0)
+    then -- output expression:
+
+      ( (x + a) `mod` w, -- pulses
+        w, -- events
+        (z' + c') `mod` w -- rotation
       )
-    else
-      error "Zero event argument not allowed"
-    where
-      w = lcm y b
-      z' = z * (div w y)
-      c' = c * (div w b)
+    else error "Zero event argument not allowed"
+  where
+    w = lcm y b
+    z' = z * div w y
+    c' = c * div w b
 
 -- Es necesaria una representación homegénea de estructura rítmica para poder
 -- aplicar un ritmo euclideano (o cualquier otro) como transformación genérica.
 
 -- composeR :: Euclid -> Euclid -> Euclid
 -- composeR = (foldl1 composeL) . (map reduceR) . (\x y -> [x,y]) -- Este patron se ve común, es instancia de qué?
-
-
