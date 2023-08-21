6e3d0d96a018e237df7b2ee0d226502e20b7829a
ninioArtillero <ixbalanque@protonmail.ch>
2023-08-21 17:18:56 -0600

    doc en inglés, refactorización, usar reduce

--- src/Sound/RTG/Geometria/Euclidean.hs
+++ src/Sound/RTG/Geometria/Euclidean.hs
@@ -1,52 +1,40 @@
-module Sound.RTG.Geometria.Euclidean where
+module Sound.RTG.Geometria.Euclidean (Euclidean, (<+>)) where
 
 type Euclidean =
-  ( Int, -- pulsos
-    Int, -- eventos
-    Int  -- rotación
+  ( Int, -- event number
+    Int, -- pulse granularity
+    Int -- rotation
   )
 
--- La siguiente función auxiliar reduce la representación de un ritmo
--- de manera que para [(x,y,z) | x >=0, y >= 0, z >= 0] se cumpla
--- (1) x <= y
--- (2) z < y
--- La cantidad de eventos, representada por y, permanece inalterada,
--- a pesar de que el ritmo pueda ser representado en menos eventos,
--- para preservar todas las posibles rotaciones.
-reduceR :: Euclidean -> Euclidean
-reduceR (x, y, z) = (x', y', z')
+-- | Produces an Euclidean representation such that
+-- the Euclidean rhythms are the set
+-- \(\{ (x,y,z) | x,y,z >= 0 , y >= z \}\).
+reduce :: Euclidean -> Euclidean
+reduce (x, y, z) = (x', y', z')
   where
-    x' =
-      if x `mod` y == 0
-        then y -- ritmos isócronos
-        else x `mod` y
-    y' = y
-    z' = z `mod` y
+    x'
+      | x == 0 = 0
+      | x `mod` y' == 0 = y'
+      | otherwise = x `mod` y'
+    y' = abs y
+    z' = z `mod` y'
 
--- Notemos que ritmos euclideanos que suenan igual pueden tener distinta representación
--- Una familia muy fácil de caracterizar es la de los ritmos isócronos:
--- (3,3), (3,6), (3,9), etc.
--- (5,5), (5,10), (5,15), etc.
+-- Isochronous rhythms have an infinite number
+-- of different representations that sound the same:
+-- (3,3), (3,6), (3,9), ...
+-- (5,5), (5,10), (5,15), ...
+-- This families are not reduced to a single element
+-- to preserve granularity.
 
--- TODO: reescribir usando guards
-(>+<) :: Euclidean -> Euclidean -> Euclidean -- type declaration
-(x, y, z) >+< (a, b, c) =
-  -- function definition
-  if (y /= 0) && (b /= 0)
-    then -- output expression:
-
-      ( (x + a) `mod` w, -- pulses
-        w, -- events
-        (z' + c') `mod` w -- rotation
-      )
-    else error "Zero event argument not allowed"
+-- | Combines two euclidean rhythms using modular arithmetic
+-- on the least common multiple of pulse granularity.
+(<+>) :: Euclidean -> Euclidean -> Euclidean -- type declaration
+r1 <+> r2
+  | (y /= 0) && (b /= 0) = ((x + a) `mod` w, w, (z' + c') `mod` w)
+  | otherwise = error "Zero pulse argument not specified"
   where
+    (a, b, c) = reduce r1
+    (x, y, z) = reduce r2
     w = lcm y b
     z' = z * div w y
     c' = c * div w b
-
--- Es necesaria una representación homegénea de estructura rítmica para poder
--- aplicar un ritmo euclideano (o cualquier otro) como transformación genérica.
-
--- composeR :: Euclid -> Euclid -> Euclid
--- composeR = (foldl1 composeL) . (map reduceR) . (\x y -> [x,y]) -- Este patron se ve común, es instancia de qué?
