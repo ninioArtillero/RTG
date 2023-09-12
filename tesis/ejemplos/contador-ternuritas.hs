02e7b7799bf79546e70eae211d5a08f0384f16f4
ninioArtillero <ixbalanque@protonmail.ch>
2023-09-12 14:20:14 -0600

    comentarios de ejemplo

--- tesis/ejemplos/contador-ternuritas.hs
+++ tesis/ejemplos/contador-ternuritas.hs
@@ -1,27 +1,33 @@
-import           Control.Concurrent
-import           Control.Monad      (forever)
+import Control.Concurrent
+import Control.Monad (forever)
 
--- Ejemplo: Utilizar un índice contador como entrada de una función
+-- Ejemplo: Utilizar un índice contador para variar la entrada de una función
+--
+-- Es algo habitual en lenguajes imperativos. Sin embargo en Haskell,
+-- contenerlo en la mónada IO creando variables mutables (MVar) con la librería
+-- Control. Concurrent
+-- Una MVar bloquea la escritura mientras su valor no haya sido extraído. Esto
+-- permite la coordinación de hilos concurrentes (creados por la función forkIO).
 
 main = do
   -- inicializar variables
   cajita <- newEmptyMVar
   index <- newMVar 0
-  -- loop infinito
+  -- loop infinito:
   -- poner ternurita en cajita
   forkIO . forever $ do
     i <- takeMVar index
-    putMVar cajita ( ternuritas i )
-    putMVar index ( i + 1 )
+    putMVar cajita (ternurita i)
+    putMVar index (i + 1)
     threadDelay $ 1000000 -- espera 1 segundo
-  -- sacar ternurita a terminal
+    -- para sacar ternurita a stdout
   forever $ do
     ternurita <- takeMVar cajita
     putStrLn ternurita
 
 -- Mi mascota par e impar
-ternuritas :: Int  -> String
-ternuritas i =
+ternurita :: Int -> String
+ternurita i =
   if (i `mod` 2 == 0)
-  then "Memuffin"
-  else "Kimbola"
+    then "Memuffin"
+    else "Kimbola"
