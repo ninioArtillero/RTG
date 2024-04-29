b348066956987bd78a630400979065829432fae2
Xavier Góngora <ixbalanque@protonmail.ch>
2024-04-29 16:52:49 -0600

    exportar Pattern calificado para evitar conflictos

--- src/Sound/RTG/Context.hs
+++ src/Sound/RTG/Context.hs
@@ -1,10 +1,10 @@
 module Sound.RTG.Context where
 
-import Sound.RTG.Geometria.Euclidean
-import Sound.RTG.Geometria.Polygon
-import Sound.RTG.Ritmo.Bjorklund
-import Sound.RTG.Ritmo.PatronRegular
-import Sound.RTG.Ritmo.Pattern
-import Sound.RTG.Ritmo.PerfectBalance
-import Sound.RTG.Ritmo.RhythmicPattern
-import Sound.RTG.Tiempo.OscOut
+import           Sound.RTG.Geometria.Euclidean
+import           Sound.RTG.Geometria.Polygon
+import           Sound.RTG.Ritmo.Bjorklund
+import           Sound.RTG.Ritmo.PatronRegular
+import qualified Sound.RTG.Ritmo.Pattern         as P
+import           Sound.RTG.Ritmo.PerfectBalance
+import           Sound.RTG.Ritmo.RhythmicPattern
+import           Sound.RTG.Tiempo.OscOut
