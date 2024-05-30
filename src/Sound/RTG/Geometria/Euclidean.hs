089aed7c6bf5081ed49b8f6cce2de6d96b6fee5f
Xavier Góngora <ixbalanque@protonmail.ch>
2024-05-30 17:25:33 -0600

    fix euclidean export

--- src/Sound/RTG/Geometria/Euclidean.hs
+++ src/Sound/RTG/Geometria/Euclidean.hs
@@ -5,7 +5,7 @@
 -- 4. Los valores negativos de k se traducen por aritmética modular sobre n.
 -- 5. Los valores negativos de n significan un cambio de orientación en el ritmo,
 --    de manera que (k,-n,p) == (k, n, -p)
-module Sound.RTG.Geometria.Euclidean (Euclidean, e, (<+>)) where
+module Sound.RTG.Geometria.Euclidean (Euclidean, e, invert) where
 
 import Data.Group
 import Sound.RTG.Ritmo.Pattern (rotateLeft)
@@ -96,7 +96,7 @@ instance Group Euclidean where
 
 -- | The interface function to construct euclidean rhythms
 -- (the value constructor is not exported).
-e :: Int -> Int -> Int -> Euclidean
-e = Euclidean
+e :: (Int, Int,  Int) -> Euclidean
+e (x,y,z) = Euclidean x y z
 
 -- TODO: Aplicar patrón de "smart constructor" y utilizar newtype para evitar errores de argumentos
