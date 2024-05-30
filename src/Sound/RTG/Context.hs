7a790319618ec1a12b0a8dcef30fd5e95c0a436a
Xavier Góngora <ixbalanque@protonmail.ch>
2024-05-30 17:26:21 -0600

    add new test module to environment

--- src/Sound/RTG/Context.hs
+++ src/Sound/RTG/Context.hs
@@ -3,6 +3,7 @@ module Sound.RTG.Context where
 import           Sound.RTG.Geometria.Euclidean
 import           Sound.RTG.Geometria.Polygon
 import           Sound.RTG.Ritmo.Bjorklund
+import           Sound.RTG.Ritmo.Combinators
 import           Sound.RTG.Ritmo.PatronRegular
 import qualified Sound.RTG.Ritmo.Pattern         as P
 import           Sound.RTG.Ritmo.PerfectBalance
