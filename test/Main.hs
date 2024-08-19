40388d4c495f6a776634dfa404471d2670fec51d
Xavier Góngora <ixbalanque@protonmail.ch>
2024-08-19 13:15:02 -0600

    update quickcheck test to current interface

--- test/Main.hs
+++ test/Main.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE FlexibleInstances #-}
 module Main (main) where
 
-import           Control.Monad                   (liftM4, unless)
+import           Control.Monad                   (unless)
 import           Sound.RTG.Ritmo.RhythmicPattern
 import           System.Exit                     (exitFailure)
 import           Test.QuickCheck
@@ -22,45 +23,43 @@ main = do
 -- which is a like a random generator for the instantiated type.
 -- It it used by default by quickCheck, verboseCheck and the like.
 -- Note that the type constructor Gen is a monad,
--- so liftM4 is used to lift the Rhythm constructor into it.
-instance Arbitrary Rhythmic where
-  arbitrary :: Gen Rhythmic
+-- so fmap is used to lift the Rhythm constructor into it.
+instance Arbitrary (Rhythm Binary) where
+  arbitrary :: Gen (Rhythm Binary)
   arbitrary = do
                 -- Larger sizes might be needed to be check with either
                 -- n <- arbitrary `suchThat` (>= 0) :: Gen Int
                 -- n <- chooseInt (0,maxBound)
-                n <- chooseInt (0,100)
-                s <- arbitrary :: Gen Sign
-                let sgn = if n == 0 then Sign 0 else s
-                liftM4 Rhythm (vector n) (return []) (return n) (return sgn)
+                n <- chooseInt (1,100)
+                fmap Rhythm (vector n)
 
 -- This instance lifts into OnsetPattern
 instance Arbitrary Binary where
   arbitrary :: Gen Binary
   arbitrary = oneof [return Zero, return One]
 
-instance Arbitrary Sign where
-  -- | 0 is not generated as it is an edge case of meter,
-  -- already accounted for on the Rhythmic instance.
-  arbitrary :: Gen Sign
-  arbitrary = frequency [(2, return $ Sign 1), (1, return $ Sign (-1))]
+-- instance Arbitrary Sign where
+--   -- | 0 is not generated as it is an edge case of meter,
+--   -- already accounted for on the Rhythmic instance.
+--   arbitrary :: Gen Sign
+--   arbitrary = frequency [(2, return $ Sign 1), (1, return $ Sign (-1))]
 
-prop_groupAssoc :: Rhythmic -> Rhythmic -> Rhythmic -> Bool
+prop_groupAssoc :: Rhythm Binary -> Rhythm Binary -> Rhythm Binary -> Bool
 prop_groupAssoc rhythmA rhythmB rhythmC =
   (rhythmA <> rhythmB) <> rhythmC == rhythmA <> (rhythmB <> rhythmC)
 
-prop_groupLeftIdentity :: Rhythmic -> Bool
+prop_groupLeftIdentity :: Rhythm Binary -> Bool
 prop_groupLeftIdentity rhythm =
   mempty <> rhythm == rhythm
 
-prop_groupRightIdentity :: Rhythmic -> Bool
+prop_groupRightIdentity :: Rhythm Binary -> Bool
 prop_groupRightIdentity rhythm =
    rhythm <> mempty == rhythm
 
-prop_groupLeftInverse :: Rhythmic -> Bool
+prop_groupLeftInverse :: Rhythm Binary -> Bool
 prop_groupLeftInverse rhythm =
   inv rhythm <> rhythm == mempty
 
-prop_groupRightInverse :: Rhythmic -> Bool
+prop_groupRightInverse :: Rhythm Binary -> Bool
 prop_groupRightInverse rhythm =
   rhythm <> inv rhythm == mempty
