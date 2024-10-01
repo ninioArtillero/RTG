8d07528c9f2e503d217aadbfab7147a3b9148346
Xavier Góngora <ixbalanque@protonmail.ch>
2024-10-01 17:18:28 -0600

    working script using shell0 with pinned nixpkgs

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Ensure that there's a persistent GC root for the Nix environment
-GC_ROOT="$(pwd)/.nix-gc-root/"
-
-# Create the GC root directory if it doesn't exist
-mkdir -p "$GC_ROOT"
-
-# Launch nix-shell with a persistent GC root
-nix-shell --add-root "$GC_ROOT"/RTG --indirect shell.nix
+# and launch cabal repl
+GC_ROOT="$(pwd)/.nix-gc-root/" &&
+    echo "Creating the GC root directory if it doesn't exist" &&
+    mkdir -p "$GC_ROOT" &&
+    echo "Launching RTG inside a nix-shell with a persistent GC root" &&
+    nix-instantiate --add-root "$GC_ROOT"/RTG shell0.nix &&
+    nix-shell shell0.nix --run 'cabal repl'
