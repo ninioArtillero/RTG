3246948a84226e8c6dcfb372a152fe447d87f5a7
Xavier Góngora <ixbalanque@protonmail.ch>
2024-10-01 17:38:41 -0600

    check TODOs to add SuperDirt to the runtime

---
+++ run1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Ensure that there's a persistent GC root for the Nix environment
+# and launch cabal repl
+GC_ROOT="$(pwd)/.nix-gc-root/" &&
+    echo "Creating the GC root directory if it doesn't exist" &&
+    mkdir -p "$GC_ROOT" &&
+    echo "Launching RTG inside a nix-shell with a persistent GC root" &&
+    nix-instantiate --add-root "$GC_ROOT"/RTG1 shell1.nix &&
+    nix-shell shell1.nix --run 'cabal repl'
