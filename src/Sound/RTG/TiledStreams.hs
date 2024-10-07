70d273efb9c1f200c616206a1d008e18990f2144
Xavier Góngora <ixbalanque@protonmail.ch>
2024-10-07 17:06:13 -0600

    reorganize and document module

--- src/Sound/RTG/TiledStreams.hs
+++ src/Sound/RTG/TiledStreams.hs
@@ -8,7 +8,6 @@
 module Sound.RTG.TiledStreams where
 
 import Sound.RTG.Music (Pitch, NoteName(..) )
-import Data.List (foldl')
 import Data.Monoid (Sum(..))
 
 -- | Tiled streams are bi-infinite sequences indexed by integers,
@@ -21,38 +20,75 @@ data TiledStream a = TiledStream { active :: RealizationInterval , sync :: SyncI
 type SyncInterval = Int
 
 -- | Represents a range where the non-zero part of a stream lies.
--- Introduced to allow recursive TiledStream definitions.
 -- It is such that the sync interval [0,d] is constained within it.
+-- NOTE: In the paper this are taken to be extended Naturals,
+-- i.e. with an Infinite value, and are part of the "sinchronization profile"
+-- (l,d,r), which are equivalent here to (-l, d+r)
 type RealizationInterval = (Int,Int)
 
 -- | A stream using integers as relative dates (time abstration)
 type Stream a =  Int -> a
 
--- | Definition of the tiled product, which generalizes both sequential
--- and parallel composition.
+instance Functor TiledStream where
+  fmap f t = t { stream = \k -> let x = stream t k in f x }
+
+-- | T-Calculus basic types are uniformly seen as semiring structures
+-- with two infix operators analogous to + and *
 instance Semigroup a => Semigroup (TiledStream a) where
-  t1 <> t2 = merge $ (+*) t1 t2
+  t1 <> t2 = merge $ t1 +* t2
 
 instance Monoid a => Monoid (TiledStream a) where
   mempty = TiledStream { active = (0,0)
                        , sync = 0
                        , stream = const mempty }
 
--- Pitch lists have a standard monoid instance, so they can be lifted
-type Pitches = [Pitch]
 
--- | The tiled product for spatio-temporal combination of tiled streams
+-- T-Calculus operations
+
+-- | The tiled product for spatio-temporal combination of tiled streams,
+-- generalizing sequential and parallel composition
 (+*) :: TiledStream a -> TiledStream b -> TiledStream (a,b)
 t1 +* t2 = 
-  TiledStream { active = productRealization t1 t2 
+  TiledStream { active = productRealization t1 t2
               , sync   = sync t1 + sync t2
               , stream = \k -> ( stream t1 k , stream t2 $ k - sync t1 ) }
+
+reset :: TiledStream a -> TiledStream a
+reset t = t { sync = 0 }
+
+coReset :: TiledStream a -> TiledStream a
+coReset t = t { sync   = 0
+              , stream = \k -> stream t $ k + sync t}
+
+merge :: Semigroup m => TiledStream (m,m) -> TiledStream m
+-- In applicative style, the new stream is defined by
+-- doing a point-wise semigroup operation of each component stream.
+merge t = t { stream = (<>) <$> stream (projection1 t) <*> stream (projection2 t) }
+
+-- Embeddings
+
+-- | List embedding
+--
+-- prop> phi (xs ++ ys) == phi xs <> phi ys
+phi :: Monoid a => [a] -> TiledStream a
+phi xs = TiledStream { active = (0, length xs)
+                     , sync = length xs
+                     , stream = \k -> if 0 <= k && k < length xs then xs !! k else mempty }
+
+-- | Stream embedding
+-- NOTE: Streams are usually thought to be defined on the natural numbers
+psi :: Monoid a => Stream a -> TiledStream a
+psi s = TiledStream { active = (0,0)
+                     , sync = 0
+                     , stream = \k -> if 0 < k then s k else mempty }
 
 -- General operations
 
 -- TODO: define higher order polyvariadic function
 -- for mapping construct
 -- https://wiki.haskell.org/Varargs
+
+{-
 
 -- | Accumulate arguments of different type
 class StreamMapping m where
@@ -64,24 +100,18 @@ instance StreamMapping (TiledStream t) where
                       , sync   = sync x + sync y
                       , stream = stream $ f x y }
 
-instance Functor TiledStream where
-  fmap f t = t { stream = \k -> let x = stream t k in f x }
 
 -- | Recursive step instance for extending arguments
 instance StreamMapping r => StreamMapping (c -> r) where
-  f x y = f (f x y) z
+  f x y c = f (f x y) c
 
--- Basic operations
+-}
+
+-- Auxiliary tiled stream functions
 
 productRealization :: TiledStream a -> TiledStream b -> RealizationInterval
 productRealization t1 t2 = let (a1,a2)= active t1; (b1,b2) = active t2
                     in (min a1 (a2 + sync t1), max a2 (b2 + sync t1))
-
-merge :: Semigroup m => TiledStream (m,m) -> TiledStream m
--- In applicative style, the new stream is defined by
--- doing a point-wise semigroup operation of each component stream.
-merge t = t { stream = (<>) <$> stream (projection1 t) <*> stream (projection2 t) }
-
 -- | The projection on the first entry lifted to a tiled stream
 projection1 :: TiledStream (a,b) -> TiledStream a
 projection1 = fmap fst
@@ -96,13 +126,6 @@ fork t1 t2 =  reset t1 +* t2
 join :: TiledStream a -> TiledStream b -> TiledStream (a,b)
 join t1 t2 = t1 +* coReset t2
 
-reset :: TiledStream a -> TiledStream a
-reset t = t { sync = 0 }
-
-coReset :: TiledStream a -> TiledStream a
-coReset t = t { sync   = 0
-              , stream = \k -> stream t $ k + sync t}
-
 -- | The monoid constraint give us means to combine values
 -- and set the stream to the default (silent) value
 -- outside the realization interval
@@ -111,14 +134,21 @@ liftVal x = TiledStream { active = (0,1)
                         , sync   = 1
                         , stream = \k -> if k == 0 then x else mempty }
 
+liftInt :: Int -> TiledStream (Sum Int)
+liftInt n = liftVal (Sum n)
+
+liftPitch :: Pitch -> TiledStream [Pitch]
+liftPitch p = liftVal [p]
+
 query :: TiledStream a -> [(a,String)]
 query t = [(stream t k, "Index:" ++ show k) | let (a,b) = active t, k <- [a..b]]
 
 -- Test examples
-x,y,z :: TiledStream Pitches
-x = liftVal [(C,4)] 
-y = liftVal [(D,5)]
-z = liftVal [(G,4)]
 
+-- | Pitch lists have a standard monoid instance, so they can be lifted
+type Pitches = [Pitch]
 
-
+c4,d5,g4 :: TiledStream Pitches
+c4 = liftPitch (C,4)
+d5 = liftPitch (D,5)
+g4 = liftPitch (G,4)
