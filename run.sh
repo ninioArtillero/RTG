9f20dda1849a3a26cb670742644ec1734d03dd3d
Xavier Góngora <ixbalanque@protonmail.ch>
2024-10-22 13:00:06 -0600

    reorganize nix files

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 # Ensure that there's a persistent GC root for the Nix environment
 # and launch cabal repl
-GC_ROOT="$(pwd)/.nix-gc-root/"
+GC_ROOT="$(pwd)/.nix-gc-roots/"
 
 if [ ! -d "$GC_ROOT" ]; then
     mkdir -p "$GC_ROOT" && echo "A gc-root directory has been created"
 fi
 
 echo "Launching RTG inside a nix-shell with a persistent GC root" &&
-    nix-instantiate --add-root "$GC_ROOT"/RTG shell0.nix &&
-    nix-shell shell0.nix --run 'cabal repl'
+    nix-instantiate --add-root "$GC_ROOT"/RTG ./nix/shell0.nix &&
+    nix-shell ./nix/shell0.nix --run 'cabal repl'
--- run1.sh
+++ run1.sh
@@ -3,12 +3,12 @@
 # Ensure that there's a persistent GC root for the Nix environment
 # and launch cabal repl
 
-GC_ROOT="$(pwd)/.nix-gc-root/"
+GC_ROOT="$(pwd)/.nix-gc-roots/"
 
 if [ ! -d "$GC_ROOT" ]; then
     mkdir -p "$GC_ROOT" && echo "A gc-root directory has been created"
 fi
 
 echo "Launching RTG inside a nix-shell with a persistent gc-root" &&
-    nix-instantiate --add-root "$GC_ROOT"/RTG1 shell1.nix &&
-    nix-shell shell1.nix
+    nix-instantiate --add-root "$GC_ROOT"/RTG1 ./nix/shell1.nix &&
+    nix-shell ./nix/shell1.nix
