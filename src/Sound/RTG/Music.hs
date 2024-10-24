df577e39b1f45b1c636802f6c12071091261fd00
Xavier Góngora <ixbalanque@protonmail.ch>
2024-10-24 17:44:20 -0600

    clash with e for euclidean, these aren't needed

--- src/Sound/RTG/Music.hs
+++ src/Sound/RTG/Music.hs
@@ -17,12 +17,3 @@ data NoteName = Cf | C | Cs | Df | D | Ds | Ef | E | Es | Ff
               | F | Fs | Gf | G | Gs | Af | A | As | Bf | B | Bs deriving (Eq, Ord, Show)
 
 type Music = Media Note
-
-cf,c,cs,df,d,ds,ef,e,es,ff,f,fs,gf,g,gs,af,a,as,bf,b,bs :: Int -> Pitch
-cf n = (Cf,n); c n = (C,n); cs n = (Cs,n)
-df n = (Df,n); d n = (D,n); ds n = (Ds,n)
-ef n = (Ef,n); e n = (E,n); es n = (Es,n)
-ff n = (Ff,n); f n = (F,n); fs n = (Fs,n)
-gf n = (Gf,n); g n = (G,n); gs n = (Gs,n)
-af n = (Af,n); a n = (A,n); as n = (As,n)
-bf n = (Bf,n); b n = (B,n); bs n = (Bs,n)
