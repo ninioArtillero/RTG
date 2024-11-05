dd58df8a8717dba0631dcddc89d42c9f7ab50602
Xavier Góngora <ixbalanque@protonmail.ch>
2024-11-05 16:36:10 -0600

    remove unnecessary module

--- src/Sound/RTG/Context.hs
+++ src/Sound/RTG/Context.hs
@@ -1,8 +1,8 @@
 module Sound.RTG.Context where
 
+import           Euterpea.Music
 import           Sound.RTG.Geometry.Euclidean
 import           Sound.RTG.Geometry.Polygon
-import           Sound.RTG.Music
 import           Sound.RTG.Rhythm.Bjorklund
 import           Sound.RTG.Rhythm.PatronRegular
 import           Sound.RTG.Rhythm.Pattern
