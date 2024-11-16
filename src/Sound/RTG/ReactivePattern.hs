1dc2f882dbe3bd720fccd698a622c8543c77dc23
Xavier Góngora <ixbalanque@protonmail.ch>
2024-11-16 16:34:11 -0600

    send superdirt messages stepwise

--- src/Sound/RTG/ReactivePattern.hs
+++ src/Sound/RTG/ReactivePattern.hs
@@ -2,9 +2,10 @@
 -- | FRP pattern implementation for on-the-fly pattern transformation
 module Sound.RTG.ReactivePattern where
 
-import           Control.Concurrent               (threadDelay)
+import           Control.Concurrent               (forkIO, threadDelay)
 import           FRP.Yampa
 import qualified Sound.Osc                        as Osc hiding (Time)
+import           Sound.Osc.Fd                     (sendMessage)
 import           Sound.RTG.Rhythm.RhythmicPattern
 import           Sound.RTG.Time.Messages
 
@@ -12,25 +13,40 @@ import           Sound.RTG.Time.Messages
 
 type Pattern = [Int]      -- Binary list of 1s (onset) and 0s (rests)
 type PatternSF = SF Time [Osc.Message]
+type Beat = Double
+type BPM = Double
+
 
 -- Function to convert a binary pattern into a repeating stream of OSC messages
-patternToSF :: Rhythmic a => a -> Double -> PatternSF
-patternToSF pttrn bpm = proc time -> do
+patternToSF :: Rhythmic a => SampleName -> Beat -> BPM -> a -> SF Time [Osc.Message]
+patternToSF sample beat bpm pttrn = proc time -> do
   let onsetPattern = getRhythm . toRhythm $ pttrn
       patternLength = length onsetPattern
-      interval = 60.0 / bpm
-  -- Compute the index within the pattern based on the current time
-  index <- arr (\(t, i, l) -> floor (t / i) `mod` l) -< (time, interval, patternLength)
+      bps = bpm / 60 -- beats per second
+  index <- arr (\(t, i, l, b) -> floor (t * i / b) `mod` l) -< (time, bps, patternLength, beat)
   -- Determine if the current index corresponds to an onset
   isOnset <- arr (\(i, onsets) -> onsets !! i == One) -< (index, onsetPattern)
+  -- Event stream
+  event <- edge -< isOnset
   -- Generate an OSC message if there's an onset
-  messages <- arr (\(t, onset) -> [oscMessage t | onset]) -< (time, isOnset)
+  messages <- arr (\e -> [superDirtMessage sample | isEvent e]) -< event
+  --messages <- arr (\(t, e) -> case e of NoEvent -> []; Event _ -> [oscMessage t]) -< (time,event)
   returnA -< messages
 
+{-
+silence :: PatternSF
+silence = constant []
+
+start = rSwitch silence
+
+play :: PatternSF
+play = (pttrnSignal &&& changePattern) `switch` (\p -> patternToSF p)
+
+
+changePattern :: Rhythmic b => SF a (Event b)
 
 -- TODO: Find a logic to generate event streams to trigger
 -- the transformation of patterns and cps
-{-
 p bpm pttrn = (patternToSF pttrn bpm &&& (holdl pttrn >>> updatePattern)) `switch` \e -> patternToSF e bpm
 
 updatePattern :: Rhythmic a => SF a (Event a)
@@ -39,7 +55,6 @@ updatePattern = proc inp -> do
                   returnA <- pttrn
 -}
 
-
 -- Function to generate an OSC message
 oscMessage :: Time -> Osc.Message
 oscMessage time = Osc.message "/playOnset" [Osc.float time]
@@ -47,23 +62,26 @@ oscMessage time = Osc.message "/playOnset" [Osc.float time]
 
 -- Reactimate setup
 
--- Initialization function: just returns initial time
+-- Initialization value. Initial time in this case
 initSense :: IO Time
 initSense = return 0
 
 -- Sense function: calculates time delta between updates
 sense :: Bool -> IO (DTime, Maybe Time)
 sense _ = do
-    threadDelay (1000000 `div` 20)  -- Update about 20 times per second
-    let deltaTime = 1 / 20
+    threadDelay (1000000 `div` 80)  -- Update about 80 times per second
+    let deltaTime = 1 / 80
     return (deltaTime, Just deltaTime)
 
 -- Actuate function: here we output the OSC messages to the console
 actuate :: Bool -> [Osc.Message] -> IO Bool
 actuate _ messages = do
-    mapM_ print messages  -- Print each message for now
+    port <- superDirtPort
+    mapM_ (sendMessage port) messages
     return False
 
 -- Run a pattern with a given BPM
-runPattern :: Rhythmic a => a -> Double -> IO ()
-runPattern pat bpm = reactimate initSense sense actuate (patternToSF pat bpm)
+runPattern :: Rhythmic a => SampleName -> Beat -> BPM -> a -> IO ()
+runPattern sample beat bpm pat = do
+  forkIO $ reactimate initSense sense actuate (time >>> patternToSF sample beat bpm pat )
+  return ()
