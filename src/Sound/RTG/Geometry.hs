c9dade706eb4d8fd9f164fd74ab9e945e0f80ff6
Xavier Góngora <ixbalanque@protonmail.ch>
2024-12-05 17:33:31 -0600

    forgot to add new submodules arrangements

---
+++ src/Sound/RTG/Geometry.hs
@@ -0,0 +1,9 @@
+module Sound.RTG.Geometry
+  (
+    module Sound.RTG.Geometry.Euclidean
+  , module Sound.RTG.Geometry.Polygon
+  )
+where
+
+import           Sound.RTG.Geometry.Euclidean
+import           Sound.RTG.Geometry.Polygon
---
+++ src/Sound/RTG/Rhythm.hs
@@ -0,0 +1,17 @@
+module Sound.RTG.Rhythm
+  (
+    module           Sound.RTG.Rhythm.Bjorklund
+  , module           Sound.RTG.Rhythm.PatronRegular
+  , module           Sound.RTG.Rhythm.Pattern
+  , module           Sound.RTG.Rhythm.PerfectBalance
+  , module           Sound.RTG.Rhythm.RhythmicPattern
+  , module           Sound.RTG.Rhythm.TimePatterns
+  )
+where
+
+import           Sound.RTG.Rhythm.Bjorklund
+import           Sound.RTG.Rhythm.PatronRegular
+import           Sound.RTG.Rhythm.Pattern
+import           Sound.RTG.Rhythm.PerfectBalance
+import           Sound.RTG.Rhythm.RhythmicPattern
+import           Sound.RTG.Rhythm.TimePatterns
