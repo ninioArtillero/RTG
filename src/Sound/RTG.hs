a7ea79264438c8a92d35efd7e181073005d0f7eb
Xavier Góngora <ixbalanque@protonmail.ch>
2024-12-06 07:28:04 -0600

    help message with use information

--- src/Sound/RTG.hs
+++ src/Sound/RTG.hs
@@ -41,3 +41,26 @@ s root rhythm =
 
 readcps :: IO CPS
 readcps = readMVar globalCPS
+
+help :: IO ()
+help = putStrLn $ unlines [
+                          "\nRTG has several funcions to generate rhythmic patterns",
+                          "that are interpreted as MIDI and OSC message streams.",
+                          "In the first case the default MIDI output device is used.",
+                          "The list of active MIDI devices can be displayed with:",
+                          "\nλ> devices\n",
+                          "For OSC message playback, an active SuperDirt instance,",
+                          "with its standard sample library loaded, is needed.",
+                          "To create a pattern use any of the following:\n",
+                          "λ> o <sample> <rhythm>          -- OSC  | play rhythm once",
+                          "λ> l <sample> <rhythm>          -- OSC  | play rhythm in loop",
+                          "λ> p <root> <rhythmA> <rhythmB> -- MIDI | play rhythmA as an scale over rhythmB (loop)",
+                          "λ> s <root> <rhythmA>           -- MIDI | play rhythmA as a scale (loop)",
+                          "\nTo stop playback make sure to bind all patterns to a name",
+                          "with the following syntax:",
+                          "\nλ> name <- ...",
+                          "λ> stop name",
+                          "\nIf a name is given again to a new pattern, the former becomes headless",
+                          "and the ghci session needs to be ended to kill all active pattern. \n",
+                          "λ> :quit"
+                          ]
