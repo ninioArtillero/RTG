72269b8ba2f429a6005cff855a41c240fa05cb50
Xavier Góngora <ixbalanque@protonmail.ch>
2025-05-02 16:34:22 -0600

    implement state persistance across ghci reloads

--- src/Sound/RTG/OscMessages.hs
+++ src/Sound/RTG/OscMessages.hs
@@ -5,15 +5,20 @@
 -- License     : GPL-3
 -- Maintainer  : ixbalanque@protonmail.ch
 -- Stability   : experimental
-module Sound.RTG.OscMessages (superDirtMessage, superDirtPort, SampleName, Dur, CPS) where
+module Sound.RTG.OscMessages (sendSuperDirtSample, superDirtMessage, superDirtPort, SampleName, Dur, CPS) where
 
-import qualified Sound.Osc as Osc
+import qualified Sound.Osc.Fd as Osc
 
 type Dur = Double
 
 type CPS = Double
 
 type SampleName = String
+
+sendSuperDirtSample :: SampleName -> IO ()
+sendSuperDirtSample sample = do
+  port <- superDirtPort
+  Osc.sendMessage port (superDirtMessage sample)
 
 superDirtMessage :: SampleName -> Osc.Message
 superDirtMessage sample =
--- src/Sound/RTG/Sequencer.hs
+++ src/Sound/RTG/Sequencer.hs
@@ -34,16 +34,22 @@ module Sound.RTG.Sequencer where
 --   d8,
 -- )
 
-import Control.Monad (forever, mapM_)
+import Control.Monad (mapM_)
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as Map
-import Data.IORef (IORef, modifyIORef', newIORef, readIORef, writeIORef)
-import Data.Maybe (fromJust, fromMaybe, isNothing)
-import Euterpea (Pitch, PitchClass (..), note, play, playS)
+import Data.Maybe (fromMaybe)
+import Euterpea (Pitch, PitchClass (..), note, play)
 import Foreign.Store
+  ( Store (..),
+    lookupStore,
+    readStore,
+    storeAction,
+    withStore,
+    writeStore,
+  )
 import qualified Sound.Osc.Fd as Osc
 import Sound.RTG.Event (Event (..), isOnset, zipValuesWithOnsets)
-import Sound.RTG.OscMessages (superDirtMessage, superDirtPort)
+import Sound.RTG.OscMessages (sendSuperDirtSample)
 import Sound.RTG.RhythmicPattern (Rhythmic, rhythm)
 import Sound.RTG.TimedMonad
   ( Micro (..),
@@ -53,7 +59,6 @@ import Sound.RTG.TimedMonad
   )
 import Sound.RTG.UnSafe (readcps)
 import Sound.RTG.Utils (patternEventDurationSec)
-import System.IO.Unsafe (unsafePerformIO)
 
 -- TODO: This module still depends on Event constructors.
 -- Can it be decoupled?
@@ -62,6 +67,7 @@ import System.IO.Unsafe (unsafePerformIO)
 
 -- | A map containing all currently running patterns.
 -- Analogous to the /bundle/ of a /fiber bundle/.
+-- TODO: Change to newtype to define custom show instance.
 type PatternPool = HashMap PatternId SequencerPattern
 
 -- | Pattern keys in a 'PatternPool' are 'Int'.
@@ -96,46 +102,56 @@ toSequencerPattern pattern outputs =
   let eventPattern = rhythm pattern
    in zipValuesWithOnsets eventPattern outputs
 
--- * PatternPool State in Stored References.
+-- * PatternPool State
 
--- | Initialize an empty pattern pool in a mutable variable.
--- It is exposed outside of 'IO' as a mutable shared state
--- to be able to update it within the ghci runtime.
-patternPoolRef :: IORef PatternPool
-patternPoolRef = unsafePerformIO $ newIORef (Map.empty)
+-- $patternpoolstate
+-- The 'PatternPool' is stored in the 'patternPoolStore', so here whe define custom
+-- 'Store' operations to manipulate it.
+-- 'Store's are used for the sequencer state to persist @ghci@ reloads.
 
-storePatternPool :: IO ()
-storePatternPool = do
-  pool <- readIORef patternPoolRef
-  writeStore (Store 0) pool
+-- | The 'Store' index for the'PatternPool'.
+patternPoolStore :: Store PatternPool
+patternPoolStore = Store 0
+
+getPatternPool :: IO PatternPool
+getPatternPool = readStore patternPoolStore
 
 queriePatterns :: IO ()
-queriePatterns = readIORef patternPoolRef >>= print . Map.keys
+queriePatterns = withStore patternPoolStore $ print . Map.keys
+
+-- | Store and return the result of an update operation on the stored value.
+updateStore :: Store a -> (a -> IO a) -> IO a
+updateStore store update = storeAction store . withStore store $ update
+
+-- | Store an empty 'PatternPool' in the 'patternPoolStore'.
+resetPatternPool :: IO ()
+resetPatternPool = writeStore patternPoolStore $ Map.empty
 
 -- | Add or update a pattern in the pattern pool.
-updatePatternPool :: PatternId -> SequencerPattern -> IO ()
-updatePatternPool id pattern = modifyIORef' patternPoolRef $ Map.insert id pattern
-
--- Test Examples:
--- updatePatternPool 1 [(Onset, Just [Osc "cp"])]
--- updatePatternPool 2 [(Onset, Just [Osc "bd", Osc "sn"]), (Onset, Just [Osc "blip"])]
--- updatePatternPool 3 [(Rest, Nothing), (Onset, Just [Osc "can"], (Rest, Nothing))]
+addPatternToPool :: PatternId -> SequencerPattern -> IO PatternPool
+addPatternToPool id pattern =
+  updateStore patternPoolStore $ pure . Map.insert id pattern
 
 -- | Remove a pattern from the pool.
-removePattern :: PatternId -> IO ()
-removePattern id = modifyIORef' patternPoolRef $ Map.delete id
+removePattern :: PatternId -> IO PatternPool
+removePattern id = updateStore patternPoolStore $ pure . Map.delete id
 
--- * Global Pattern. Analogous to the /base space/ of a /fiber bundle/.
+-- * Global Pattern
+
+-- $globalpattern
+-- Analogous to the /base space/ of a /fiber bundle/. The 'globalPattern' is the means
+-- to play the patterns contained in the 'PatternPool'. The /merging/ proceduce it
+-- implements is analogous to a /projection/.
 
 -- | The global pattern obtained from merging all patterns in the pool.
 globalPattern :: IO (SequencerPattern)
-globalPattern = do
-  patternPool <- readIORef patternPoolRef
-  let patternLengthsMap = Map.map (length) patternPool
-      leastCommonMultiple = Map.foldl' lcm 1 patternLengthsMap
-      adjustedPatterns = Map.map (alignPattern leastCommonMultiple) patternPool
-      gp = Map.foldl' (matchOutputEvents) [] adjustedPatterns
-  pure gp
+globalPattern = withStore patternPoolStore $
+  \patternPool -> do
+    let patternLengthsMap = Map.map (length) patternPool
+        leastCommonMultiple = Map.foldl' lcm 1 patternLengthsMap
+        adjustedPatterns = Map.map (alignPattern leastCommonMultiple) patternPool
+        gp = Map.foldl' (matchOutputEvents) [] adjustedPatterns
+    pure gp
 
 {-@ alignPattern :: Integral a => n:a
                  -> {pttrn : SequencerPattern | n `mod` (length pttrn) == 0 }
@@ -162,24 +178,38 @@ matchOutputEvents pttrn pttrn' = zipWith f pttrn pttrn'
         then (Onset, Just $ (fromMaybe [] outputs) ++ (fromMaybe [] outputs'))
         else (Rest, Nothing)
 
--- * Play state
+-- * Play Status
 
--- | Current play status. 'Nothing' stands for no playback.
--- A value gives the current playback process reference.
-playRef :: IORef (Maybe (Ref TIO ()))
-playRef = unsafePerformIO $ newIORef Nothing
+-- $playstatus
+-- The 'playStatus' is inferred from a 'Maybe (Ref TIO ())' (a 'TimedMonad' reference)
+-- value inside a 'Store'.
+-- Under the hood, the reference carries a 'Control.Concurrency.threadId'
+-- as a process handle. 'Nothing' stands for no-playback.
 
-storePlayState :: IO ()
-storePlayState = do
-  status <- readIORef playRef
-  writeStore (Store 1) status
+-- | Current play thread reference store. 'Nothing' stands for no-playback.
+playThreadStore :: Store (Maybe (Ref TIO ()))
+-- playThreadStore = unsafePerformIO $ newStore $ Nothing
+playThreadStore = Store 1
+
+-- | Updates the 'playThreadStore' with the given reference and
+-- returns both the old and new playing thread references.
+updatePlayThread :: Maybe (Ref TIO ()) -> IO (Maybe (Ref TIO ()), Maybe (Ref TIO ()))
+updatePlayThread currentlyPlaying = do
+  wasPlaying <- readStore playThreadStore
+  writeStore playThreadStore currentlyPlaying
+  pure (wasPlaying, currentlyPlaying)
+
+getPlayThread :: IO (Maybe (Ref TIO ()))
+getPlayThread = do
+  currentlyPlaying <- readStore playThreadStore
+  pure currentlyPlaying
 
 playStatus :: IO String
 playStatus = do
-  currentlyPlaying <- readIORef playRef
-  if isNothing currentlyPlaying
-    then pure "Nothing is playing"
-    else pure "Just playing"
+  withStore playThreadStore $ \currentlyPlaying -> do
+    case currentlyPlaying of
+      Nothing -> pure "Nothing is playing"
+      Just _ -> pure "Just playing"
 
 -- * Play Functionality
 
@@ -188,13 +218,36 @@ playStatus = do
 -- managing computation time drifts explicitly and get (approximately)
 -- correct timing.
 
--- | Play through the Timed IO Monad 'TIO' in a loop.
--- The 'Micro' parameter should be generated from the global cps value;
--- it defines the duration of each event.
+-- | Play a pattern in the Timed IO Monad 'TIO'.
+-- The playback is in a loop and runs on a forked thread.
+-- This functions is called at 'refreshSequencerPattern'
+-- to play the current 'globalPattern'.
+-- There the 'Micro' parameter corresponds to the duration of each event,
+-- and is derived from the global cps value and the pattern length.
 playSequencerPattern :: Micro -> SequencerPattern -> TIO ()
-playSequencerPattern dur sp = do
-  ref <- fork $ mapM_ (eventOutput dur . snd) $ cycle sp
-  lift $ writeIORef playRef (Just ref)
+playSequencerPattern dur sp =
+  if dur == 0 || null sp
+    then do
+      (wasPlaying, _) <- lift $ updatePlayThread Nothing
+      case wasPlaying of
+        Nothing -> pure ()
+        Just thread -> freeze thread
+    else do
+      wasPlaying <- lift $ getPlayThread
+      case wasPlaying of
+        Nothing -> do
+          thread <- fork $ patternOutput dur sp
+          _ <- lift $ updatePlayThread (Just thread)
+          pure ()
+        Just oldThread -> do
+          freeze oldThread
+          delay dur
+          newThread <- fork (patternOutput dur sp)
+          _ <- lift $ updatePlayThread (Just newThread)
+          pure ()
+
+patternOutput :: Micro -> [(a, Maybe [Output])] -> TIO ()
+patternOutput dur sp = mapM_ (eventOutput dur . snd) $ cycle sp
 
 -- | Generates an event playback by the given duration inside the timed IO monad.
 eventOutput :: Micro -> Maybe [Output] -> TIO ()
@@ -203,9 +256,7 @@ eventOutput dur (Just outputs) = mapM_ (\x -> instantOut dur x >> delay dur) out
 
 -- | TODO: output for Note is messing up time. For the mean time I'll forget about it.
 instantOut :: Micro -> Output -> TIO ()
-instantOut dur (Osc sample) = lift $ do
-  port <- superDirtPort
-  Osc.sendMessage port (superDirtMessage sample)
+instantOut dur (Osc sample) = lift $ sendSuperDirtSample sample
 instantOut dur (Note pitch) =
   let Micro m = dur
       microsecs = fromIntegral m
@@ -213,34 +264,34 @@ instantOut dur (Note pitch) =
    in lift $ do
         play $ note secs pitch -- too slow to catch up?
 
--- * Managing the Sequencer State
+-- * Sequencer State
 
+-- | Wrapper for operations that depend on the sequencer state.
 inSequencer :: IO a -> IO a
 inSequencer action = do
+  storeInit
   returnValue <- action
   refreshSequencer
-  storePatternPool
-  storePlayState
   pure returnValue
 
--- | Querie the environment and re-trigger excecution.
+-- | Initializes the 'patternPoolStore' and the 'playThreadStore'.
+storeInit :: IO ()
+storeInit = do
+  [maybePoolStore, maybeThreadStore] <- mapM lookupStore [0, 1]
+  case (maybePoolStore, maybeThreadStore) of
+    (Nothing, Nothing) -> resetPatternPool >> writeStore playThreadStore Nothing
+    (Nothing, _) -> resetPatternPool
+    (_, Nothing) -> writeStore playThreadStore Nothing
+    (_, _) -> pure ()
+
+-- | Querie the environment and (re-)trigger execution.
 refreshSequencer :: IO ()
 refreshSequencer = do
   gp <- globalPattern
   cps <- readcps
-  currentlyPlaying <- readIORef playRef
-  let grain = length gp
-      dur =
-        if grain /= 0 -- We get this at the start of after clearing the pool
-          then cpsToTimeStamp cps grain
-          else 0
-  if isNothing currentlyPlaying
-    then run $ playSequencerPattern dur gp -- resume
-    else run $ do
-      -- kill running sequencer pattern and resume
-      freeze (fromJust currentlyPlaying)
-      delay dur
-      playSequencerPattern dur gp
+  let len = length gp
+      dur = if len /= 0 then cpsToTimeStamp cps len else 0
+  run $ playSequencerPattern dur gp
 
 cpsToTimeStamp :: (RealFrac a, Integral b) => a -> b -> Micro
 cpsToTimeStamp cps pttrnLen = Micro . round $ 1 / (toRational cps * fromIntegral pttrnLen) * 10 ^ 6
@@ -248,7 +299,7 @@ cpsToTimeStamp cps pttrnLen = Micro . round $ 1 / (toRational cps * fromIntegral
 -- * Execution Interface
 
 -- | Default pattern ID following the Tidal Cycles idiom.
-d1, d2, d3, d4, d5, d6, d7, d8 :: (Rhythmic a) => [SampleName] -> a -> IO ()
+d1, d2, d3, d4, d5, d6, d7, d8 :: (Rhythmic a) => [SampleName] -> a -> IO PatternPool
 d1 samples = playPattern 1 (map (\s -> [Osc s]) samples)
 d2 samples = playPattern 2 (map (\s -> [Osc s]) samples)
 d3 samples = playPattern 3 (map (\s -> [Osc s]) samples)
@@ -261,37 +312,35 @@ d8 samples = playPattern 8 (map (\s -> [Osc s]) samples)
 -- | Entry point for a pattern execution. It add the pattern to the pool
 -- and updates the sequencer.
 -- TODO: Add argument to modify event matching strategy for output values.
-playPattern :: (Rhythmic a) => PatternId -> [[Output]] -> a -> IO ()
+playPattern :: (Rhythmic a) => PatternId -> [[Output]] -> a -> IO PatternPool
 playPattern id outputs pattern = inSequencer $ do
   let newSequencerPattern = toSequencerPattern pattern outputs
-  updatePatternPool id newSequencerPattern
+  addPatternToPool id newSequencerPattern
 
 -- | Convinience alias. Used to resume play after 'stopAll'.
 resume :: IO ()
 resume = refreshSequencer
 
 -- | Removes a pattern both from the pool and playback.
-kill :: PatternId -> IO ()
-kill id = inSequencer $ do
-  removePattern id
-  pool <- readIORef patternPoolRef
-  if pool == Map.empty
-    then modifyIORef' playRef $ const Nothing
-    else pure ()
+kill :: PatternId -> IO PatternPool
+kill = inSequencer . removePattern
 
 -- | Clear the pool and stop playing.
 clear :: IO ()
-clear = inSequencer $ do
+clear = do
   stopAll
-  modifyIORef' playRef $ const Nothing
-  modifyIORef' patternPoolRef $ const Map.empty
+  resetPatternPool
 
 -- | Stop playing, but keep patterns in the pool.
 stopAll :: IO ()
-stopAll = inSequencer $ run go
+stopAll = run go
   where
     go :: TIO ()
     go = do
-      currentlyPlaying <- lift $ readIORef playRef
-      freeze $ fromJust currentlyPlaying
-      lift $ modifyIORef' playRef $ const Nothing
+      currentlyPlaying <- lift $ getPlayThread
+      case currentlyPlaying of
+        Nothing -> lift $ putStrLn "Nothing is playing in silence loudly, listen..."
+        Just thread -> do
+          freeze thread
+          _ <- lift $ updatePlayThread Nothing
+          pure ()
